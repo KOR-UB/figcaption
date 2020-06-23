a9aa39c5aa629376e5256a0f0744050245da7ec1
readminimo <jjom22ww@gmail.com>
2020-06-23 11:44:07 +0900

    I fixed some tag indexHtml

--- public/js/upload.js
+++ public/js/upload.js
@@ -2,6 +2,7 @@ const $header = document.querySelector('header');
 const $main = document.querySelector('main');
 const $postAdd = document.querySelector('.post-add');
 const $post = document.querySelector('.post');
+const $btnCancle = document.querySelector('.btn-cancle');
 
 function readURL(input) {
   if (input.files && input.files[0]) {
@@ -14,9 +15,17 @@ function readURL(input) {
   }
 }
 $postAdd.onclick = () => {
-  console.log('hi');
+  togglePopup();
+}
+$btnCancle.onclick = () => {
+  togglePopup();
+}
+
+function togglePopup() {
   $post.classList.toggle('post-active')
 }
+
+//if startsWith="/img"
 // export function bindEvent() {
 //   const $inputFile = $main.querySelector(".input-file");
 //   $inputFile.addEventListener("change", e => {
