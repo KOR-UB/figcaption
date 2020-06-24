da4e3c118f87fc13e9739c155085128853d23a85
= <yyb2535@gmail.com>
2020-06-24 14:05:17 +0900

    feat: feed.js, index.html

--- public/js/feed.js
+++ public/js/feed.js
@@ -67,13 +67,33 @@ function feedDomNodeSettings() {
 
 function createNode(tag, _class = '') {
   const node = document.createElement(tag);
+  if (tag === 'button' && _class === 'heart') {
+    node.innerHTML = `
+    <svg fill="#ed4956;" height="22" viewBox="0 0 48 48" width="22">
+    <path
+      d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z">
+    </path>
+  </svg>`;
+  } else if (tag === 'button' && _class === 'msg') {
+    node.innerHTML = `
+    <svg fill="#262626" height="22" viewBox="0 0 48 48" width="22">
+    <path clip-rule="evenodd"
+      d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z"
+      fill-rule="evenodd"></path>
+  </svg>`;
+  } else if (tag === 'button' && _class === 'mark') {
+    node.innerHTML = `
+    <svg fill="#262626" height="22" viewBox="0 0 48 48" width="22">
+    <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"></path>
+  </svg>`;
+  }
   node.className = _class;
   return node;
 }
 
 function postRender(board, user) {
   console.log(board);
-  const { imgList } = board;
+  const { imgList, hashList } = board;
   console.log(imgList);
   console.log(user);
   const $postCard = createNode('li', 'post-card'),
@@ -95,20 +115,20 @@ function postRender(board, user) {
   
   $leftIcon = createNode('div', 'left-icon'),
   $heart = createNode('button', 'heart'),
-  $heartSvg = createNode('svg'),
-  $heartPath = createNode('path'),
+  // $heartSvg = createNode('svg'),
+  // $heartPath = createNode('path'),
 
   $msg = createNode('button', 'msg'),
-  $msgSvg = createNode('svg'),
-  $msgPath = createNode('path'),
+  // $msgSvg = createNode('svg'),
+  // $msgPath = createNode('path'),
 
   $centerIcon = createNode('div', 'center-icon'),
   // $itemLight = createNode('div', 'item-light'),
 
   $rightIcon = createNode('div', 'right-icon'),
   $mark = createNode('button', 'mark'),
-  $markSvg = createNode('svg'),
-  $markPath = createNode('path'),
+  // $markSvg = createNode('svg'),
+  // $markPath = createNode('path'),
 
   $postTextContainer = createNode('div', 'post-text-container'),
   $heartText = createNode('p', 'heart-text'),
@@ -119,7 +139,8 @@ function postRender(board, user) {
   $longText = createNode('div', 'long-text');
 
 
-  
+  $profile.src = 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=ZMRBdU8i2AoAX_wbci2&oh=12c3cd55d80ceadd2d32df292f236937&oe=5F13AC8F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2';
+  $postNickName.textContent = user.nickName;
   $postHeader.appendChild($profile);
   $postHeader.appendChild($postNickName);
   
@@ -134,10 +155,10 @@ function postRender(board, user) {
   $slideController.appendChild($sliderLeftBtn);
   $slideController.appendChild($sliderRightBtn);
   $postImgContainer.appendChild($slideController);
-  $heartSvg.appendChild($heartPath);
-  $heart.appendChild($heartSvg);
-  $msgSvg.appendChild($msgPath);
-  $msg.appendChild($msgSvg);
+  // $heartSvg.appendChild($heartPath);
+  // $heart.appendChild($heartSvg);
+  // $msgSvg.appendChild($msgPath);
+  // $msg.appendChild($msgSvg);
   $leftIcon.appendChild($heart);
   $leftIcon.appendChild($msg);  
   $postIconContainer.appendChild($leftIcon);
@@ -146,23 +167,29 @@ function postRender(board, user) {
     if (imgList.length === 1) return;
     const $itemLight = createNode('div', 'item-light');
     $centerIcon.appendChild($itemLight); //pos;
-    $centerIcon.firstElementChild.classList.add("active");
   })
+  $centerIcon.firstElementChild.classList.add("active");
   
   $postIconContainer.appendChild($centerIcon);
-
-  $markSvg.appendChild($markPath);
-  $mark.appendChild($markSvg)
+  
+  // $markSvg.appendChild($markPath);
+  // $mark.appendChild($markSvg)
   $rightIcon.appendChild($mark);
   $postIconContainer.appendChild($rightIcon);
 
   $postContent.appendChild($postIconContainer);
 
+  $heartText.textContent = `좋아요`;
+  $count.textContent = ` ${board.likeCount}개`;
   $postTextContainer.appendChild($heartText);
   $heartText.appendChild($count);
+  $nick.textContent = user.nickName;
   $postTextContainer.appendChild($nick);
+  $hash.textContent = hashList[0].value;
+  $plusText.textContent = ' 더 보기';
   $nick.appendChild($hash);
   $nick.appendChild($plusText);
+  $longText.textContent = board.content;
   $postTextContainer.appendChild($longText);
   $postContent.appendChild($postTextContainer);
 
